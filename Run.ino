18b7fcab0647097f72b1fc5e34e4ddc99e9adb9f
jortvangorkum <jortvangorkum@gmail.com>
2018-02-12 15:58:13 +0100

    gitignore added and improved timer

--- Run.ino
+++ Run.ino
@@ -1,7 +1,9 @@
-int ledPin = 8;
-int shortTimeOn = 300;
-int longTimeOn = 1000;
-int timeOff = 500;
+const int ledPin = 8;
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+const long shortInterval = 300;
+const long longInterval = 1000;
+int timeOffInterval = 500;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
@@ -9,30 +11,56 @@ void setup() {
 }
 
 void loop() {
-    blinkLed(LED_BUILTIN, 1000, 1000);
-    SOS();
+    blinkLed(LED_BUILTIN, 500, 500);
+    //SOS();
 }
 
 
 void SOS() {
   // Loops 3 times short blink
-  for (int t = 0; t < 2; t++) {ß
-    blinkLed(ledPin, shortTimeOn, timeOff);
+  for (int t = 0; t < 2; t++) {
+    blinkLed(ledPin, shortInterval, timeOffInterval);
   }
   // Loops 3 times long blink
   for (int t = 0; t < 2; t++) {
-    blinkLed(ledPin, longTimeOn, timeOff);
+    blinkLed(ledPin, longInterval, timeOffInterval);
   }
   // Loops 3 times short blink
   for (int t = 0; t < 2; t++) {
-    blinkLed(ledPin, shortTimeOn, timeOff);
+    blinkLed(ledPin, shortInterval, timeOffInterval);
   }
   delay(2000);
 }
 
 void blinkLed(int ledPin, int firstDelay, int secondDelay) {
-  digitalWrite(ledPin, HIGH);
-  delay(firstDelay);
-  digitalWrite(ledPin, LOW);
-  delay(secondDelay);
+  // currentMillis = millis();
+
+  // if (currentMillis - previousMillis >= firstDelay) {
+  //   previousMillis = currentMillis;
+
+  //   digitalWrite(ledPin, HIGH);
+  //   if (currentMillis - previousMillis >= secondDelay) {
+  //     previousMillis = currentMillis;
+
+  //     digitalWrite(ledPin, LOW);
+  //   }
+  // }
+
+  timerSwitchLedState(ledPin, LOW, firstDelay);
+  timerSwitchLedState(ledPin, HIGH, secondDelay);
+}
+
+void timerSwitchLedState(int ledPin, int ledState, long interval) {
+  currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    digitalWrite(ledPin, ledState);
+  }
 }
